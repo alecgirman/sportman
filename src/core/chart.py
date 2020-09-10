33da8d5e4038dabf2d721098c66962813d0e5e42
Alec Girman <avgch1@gmail.com>
2020-09-10 11:32:59 -0400

    Feat: Yahoo Finance JSON API
    Uses the JSON API found in Yahoo finance instead of scraping the
    webpage.  This method is also significantly faster.
    
    Fixes #4

--- src/core/chart.py
+++ src/core/chart.py
@@ -1,12 +1,15 @@
 import matplotlib.pyplot as plt
-import sys
+import sys, os
 import requests
 import pandas as pd
 from datetime import datetime, timedelta
 
+sys.path.append(os.path.realpath('.'))
+from util import logger
 
 def build_url(ticker: str, start: datetime, end: datetime,
               interval: str, include_prepost: bool):
+    logger.trace(f'build_url()')
     base_url = f'https://query1.finance.yahoo.com/v8/finance/chart/{ticker}?'
 
     # only verify 
@@ -24,19 +27,51 @@ def build_url(ticker: str, start: datetime, end: datetime,
     e = 'events=div|split|earn&'
     f = 'lang=en-US'
 
-    return base_url + s + a + b + c + d + e + f
+    url =  base_url + s + a + b + c + d + e + f
+    logger.debug(f'build_url: Returning url {url}')
+    return url
+
+def try_request(url):
+    print(url)
+    try:
+        logger.debug(f'try_request: Trying HTTP get for url {url}')
+        data = requests.get(url)
+        return data
+    except:
+        logger.fatal(f'try_request: Exception occurred in requests.get()')
+
+def get_raw_json(ticker: str, start: datetime, end: datetime,
+              interval: str, include_prepost: bool):
+    logger.trace(f'get_raw_json({ticker}, ...)')
+    url = build_url(ticker, start, end, interval, include_prepost)
+    req = try_request(url)
+    return req.json()
 
 def get_chart(ticker: str, start: datetime, end: datetime,
               interval: str, include_prepost: bool):
-    url = build_url(ticker, start, end, interval, include_prepost)
-    req = requests.get(url)
-    return req.json()['chart']['result'][0]
+    logger.trace(f'get_chart({ticker}, ...)')
+    json = get_raw_json(ticker, start, end, interval, include_prepost)
+    return json['chart']['result'][0]
 
-def get_day_chart(ticker):
+def build_simple_url(ticker):
+    '''
+    Builds a simple url for when start/end is not needed
+    For example: retrieving the current price
+    '''
+    logger.trace(f'build_simple_url({ticker})')
     now = datetime.now()
     yst = now - timedelta(1)
     url = build_url(ticker, yst, now, '1m', True)
-    req = requests.get(url)
+    logger.debug(f'build_simple_url: Return url: {url}')
+    return url
+
+def get_day_chart(ticker):
+    logger.trace(f'get_day_chart({ticker})')
+    req = try_request(build_simple_url(ticker))
     return req.json()['chart']['result'][0]
 
-print(get_day_chart('AMD'))
+def get_stock_price(ticker):
+    logger.trace(f'get_stock_price({ticker})')
+    mktprice = get_day_chart(ticker)['meta']['regularMarketPrice']
+    logger.debug(f'get_stock_price: Market price of {ticker} is {mktprice}')
+    return mktprice
